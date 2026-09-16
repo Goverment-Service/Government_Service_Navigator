1db767eb18902e4b67ead4188d6caaf28771de58
Parami2003-04-13 <sudharmaparami2003@gmail.com>
2026-09-16 12:02:59 +0530

    Add IRefundService interface

---
+++ backend/src/Services/Interfaces/IRefundService.cs
@@ -0,0 +1,14 @@
+using Government_Service_Navigator.Backend.Models.Entities;
+
+namespace Government_Service_Navigator.Backend.Services.Interfaces
+{
+    public interface IRefundService
+    {
+        Task<RefundRequest> CreateRefundRequestAsync(int paymentId, decimal amount, string reason, string requestedByEmail);
+        Task<RefundRequest?> GetRefundByIdAsync(int id);
+        Task<RefundRequest> ApproveAsync(int id, string decidedByEmail, string? note);
+        Task<RefundRequest> RejectAsync(int id, string decidedByEmail, string? note);
+        Task<RefundRequest> ProcessAsync(int id, string transactionRef);
+        Task<RefundRequest> CompleteAsync(int id);
+    }
+}
